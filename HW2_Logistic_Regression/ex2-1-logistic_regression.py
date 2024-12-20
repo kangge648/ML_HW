390627ee8bb843125f4384dc929f8015496a8d16
Kangge648 <3097028895@qq.com>
2024-12-20 19:23:32 +0800

    update by my_computer: update HW2

--- HW2_Logistic_Regression/ex2-1-logistic_regression.py
+++ HW2_Logistic_Regression/ex2-1-logistic_regression.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
+# sigmoid function, z is a scalar
 def Sigmoid(z):
     return 1/(1 + np.exp(-z))
 
@@ -11,20 +11,23 @@ def Sigmoid(z):
 # ax.plot(nums, sigmoid(nums), 'r')
 # plt.show()
 
-def computeCost(X, Y, theta, i):
-    # if i == 1:
-    #     print(Y.T)
-    #     print(np.log(Sigmoid(X @ theta.T)))
-    inner = - Y.T @ np.log(Sigmoid(X @ theta.T)) - (1 - Y.T) @ np.log(1 - Sigmoid(X @ theta.T))
+# compute the loss
+# input (dimensions): X is m, n+1; Y is m, 1; theta is n+1, 1
+# output: a scalar
+def computeCost(theta, X, Y):
+    inner = np.sum(- Y.T @ np.log(Sigmoid(X @ theta)) - (1 - Y.T) @ np.log(1 - Sigmoid(X @ theta)))
     return inner / len(X)
 
+# gradient desent
+# input (dimensions): X is m, n+1; Y is m, 1; theta is n+1, 1; alpha and iteration are scalars
+# output: theta is n+1, 1; lossses
 def gradient_descent(X, Y, theta, alpha, iteration, losses):
+    Y = Y.reshape(-1, 1)
     for i in range(iteration):
-        Y_pred = Sigmoid(X @ theta.T)
-        loss = computeCost(X, Y, theta, i)
+        loss = computeCost(theta, X, Y)
         losses.append(loss)
-        gradient = 1/len(X) * X.T * (Y_pred - Y)
-        theta -= alpha * gradient.T
+        gradient = 1/len(X) * X.T @ (Sigmoid(X @ theta) - Y)
+        theta -= alpha * gradient
 
     return theta, losses
 
@@ -44,19 +47,21 @@ negative = data[data['Ispermitted'].isin([0])]
 # ax.set_ylabel('Exam2 Score')
 # plt.show()
 
-# 建立输入输出，得到theta
+# 建立输入输出
 data.insert(0, 'Ones', 1)
 cols = data.shape[1]
-X = data.iloc[:, 0:cols-1]
-Y = data.iloc[:, cols-1:cols]
-X = np.matrix(X.values)
-Y = np.matrix(Y.values)
-theta = np.matrix(np.array([0.0, 0.0, 0.0]))
-alpha = 0.01
-losses = []
-iteration = 1000
-theta, losses = gradient_descent(X, Y, theta, alpha, iteration, losses)
-print(type(losses))
+X = data.iloc[:, 0:-1].values
+Y = data.iloc[:, -1].values
+theta = np.array([[0.0], [0.0], [0.0]])
+
+# # 只用自己编写的梯度下降法，需要alpha很小，而iteration很大才行
+# # 也可以选用内置优化方法
+# # --------------------------自行编写求解-------------------------
+# alpha = 0.001
+# losses = []
+# iteration = 100000
+# theta, losses = gradient_descent(X, Y, theta, alpha, iteration, losses)
+# print(theta)
 
 # plt.figure(figsize=(12, 8))
 # plt.xlabel('Iteration')
@@ -64,3 +69,39 @@ print(type(losses))
 # plt.plot(range(iteration), losses, color='blue')
 # plt.title('Cost Function by Iteration')
 # plt.show()
+# # --------------------------自行编写求解-------------------------
+
+
+# --------------------------内置优化函数-------------------------
+def gradient(theta, X, Y):
+    return 1/len(X) * X.T @ (Sigmoid(X @ theta) - Y)
+
+import scipy.optimize as opt
+res = opt.minimize(fun=computeCost, x0=np.array(theta), args=(X, np.array(Y)), method='Newton-CG', jac=gradient)
+theta = res.x
+print(theta)
+# --------------------------内置优化函数-------------------------
+
+# 验证
+def predict(theta, X):
+    probability = Sigmoid(X @ theta)
+    return [1 if x >= 0.5 else 0 for x in probability]
+
+predictions = predict(theta, X)
+correct = [1 if a^b == 0 else 0 for (a,b) in zip(predictions, Y)]
+accuracy = (sum(correct) / len(correct))
+print('accuracy = {0:.0f}%'.format(accuracy*100))
+
+# 决策边界
+coef = -theta / theta[2]
+x = np.arange(30, 100, 0.5)
+y = coef[0] + coef[1] * x
+
+fig, ax = plt.subplots(figsize=(12, 8))
+ax.scatter(positive['Score1'], positive['Score2'], s=50, c='b', marker='o', label='Admitted')
+ax.scatter(negative['Score1'], negative['Score2'], s=50, c='r', marker='x', label='Not Admitted')
+ax.plot(x, y, label='Decision Boundary', c='grey')
+ax.legend()
+ax.set_xlabel('Exam1 Score')
+ax.set_ylabel('Exam2 Score')
+plt.show()
--- HW2_Logistic_Regression/test.py
+++ HW2_Logistic_Regression/test.py
@@ -1,8 +1,4 @@
 import numpy as np
 
-X = np.array([3, 1])
-theta = np.array([[1, 4]])
-
-Z = X @ theta.T
-print(Z)
-print(1/(1 + np.exp(Z)))
+X = np.matrix(np.array([[3], [1]]))
+print(2 * X)
